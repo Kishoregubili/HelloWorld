f8e32760d2c59b30017b66ae839ba3634572eb99
kishoregubili <crishna.yt1@gmail.com>
2025-03-13 10:54:25 +0530

    first commit

---
+++ src/test/java/org/kishore/helloworld/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kishore.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
